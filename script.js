d8909da7e223a43450eab9396330a3139115b1f2
Nicolas Vivas <nicolasvivas93@gmail.com>
2022-08-11 17:03:17 -0300

    Create hover effect with event listener (mouseover)

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
+let squaresNum;
+
 const container = document.querySelector(".container")
 
-for (let i = 1; i <= 256; i++) {
-    const divsGrid = document.createElement('div')
-    divsGrid.classList.toggle("box");
-    divsGrid.innerText = "dada"
-    container.appendChild(divsGrid);
+if (squaresNum !== undefined) {
+    for (let i = 1; i <= (squaresNum * squaresNum); i++) {
+        const divsGrid = document.createElement('div')
+        divsGrid.classList.toggle("box");
+        divsGrid.innerText = "dada"
+        container.appendChild(divsGrid);
+    }
+} else {
+    for (let i = 1; i <= 256 ; i++) {
+        const divsGrid = document.createElement('div')
+        divsGrid.classList.toggle("box");
+        divsGrid.innerText = "dada"
+        container.appendChild(divsGrid);
+    }
 }
 
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    squaresNum = prompt("Squares per side: ")
+});
+
+
 console.log(container)
+
+const boxes = document.querySelectorAll(".box");
+boxes.forEach(box => box.addEventListener("mouseover", () => {
+    box.classList.add("change")
+}));
+
