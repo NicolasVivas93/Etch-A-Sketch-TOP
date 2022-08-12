41c437960d821ba857b93ceb2eadadcfa58fdf7a
Nicolas Vivas <nicolasvivas93@gmail.com>
2022-08-12 15:44:33 -0300

    Create function to create grid

--- script.js
+++ script.js
@@ -2,28 +2,27 @@ let squaresNum;
 
 const container = document.querySelector(".container")
 
-if (squaresNum !== undefined) {
-    for (let i = 1; i <= (squaresNum * squaresNum); i++) {
-        const divsGrid = document.createElement('div')
-        divsGrid.classList.toggle("box");
-        divsGrid.innerText = "dada"
-        container.appendChild(divsGrid);
-    }
-} else {
-    for (let i = 1; i <= 256 ; i++) {
-        const divsGrid = document.createElement('div')
-        divsGrid.classList.toggle("box");
-        divsGrid.innerText = "dada"
-        container.appendChild(divsGrid);
-    }
+for (let i = 1; i <= 256 ; i++) {
+    const divsGrid = document.createElement('div')
+    divsGrid.classList.toggle("box");
+    divsGrid.innerText = "dada"
+    container.appendChild(divsGrid);
 }
-
 
 const button = document.querySelector('button');
 button.addEventListener('click', () => {
     squaresNum = prompt("Squares per side: ")
+    createGrid(squaresNum)
 });
 
+function createGrid(num) {
+    for (let i = 1; i <= (num * num); i++) {
+        const divsGrid2 = document.createElement('div')
+        divsGrid2.classList.toggle("box");
+        divsGrid2.innerText = "dada" 
+        container.appendChild(divsGrid2);
+    }
+}
 
 console.log(container)
 
