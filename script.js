544162a5a5e88f7b7a60c3d9bcaf1d8156d747dd
Nicolas Vivas <nicolasvivas93@gmail.com>
2022-08-15 13:14:12 -0300

    Add functionality to remove child and create new grid

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-let squaresNum;
-
 const container = document.querySelector(".container")
 
-for (let i = 1; i <= 256 ; i++) {
-    const divsGrid = document.createElement('div')
-    divsGrid.classList.toggle("box");
-    divsGrid.innerText = "dada"
-    container.appendChild(divsGrid);
+let squaresNum = 16;
+createGrid(squaresNum)
+
+function createGrid(num) {
+    for (let i = 1; i <= (num * num); i++) {
+        const divsGrid = document.createElement('div')
+        divsGrid.classList.toggle("box");
+        divsGrid.innerText = "dada" 
+        container.appendChild(divsGrid);
+    }
 }
+
+
+const childs = document.querySelectorAll(".box")
+console.log(childs)
 
 const button = document.querySelector('button');
 button.addEventListener('click', () => {
+    childs.forEach(child => {
+        container.removeChild(child)
+    })
     squaresNum = prompt("Squares per side: ")
     createGrid(squaresNum)
 });
 
-function createGrid(num) {
-    for (let i = 1; i <= (num * num); i++) {
-        const divsGrid2 = document.createElement('div')
-        divsGrid2.classList.toggle("box");
-        divsGrid2.innerText = "dada" 
-        container.appendChild(divsGrid2);
-    }
-}
-
-console.log(container)
 
 const boxes = document.querySelectorAll(".box");
 boxes.forEach(box => box.addEventListener("mouseover", () => {
